35f7d1fe05fd81fba0dfe68df84e6389ef99f43d
Sohail Ahmad <110610119+Sohaildir@users.noreply.github.com>
2023-06-06 14:47:26 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
-let hour=prompt("enter thre time");
+tofindgrade();
+function tofindgrade(){
+let marks=prompt("enter the marks")
+if(marks>=80&&marks<=100)
+{
+    document.write("you got the  A grade and your marks is ",marks )
+}
+else if (marks>=75&&marks<80)
+{
+    document.write("you got the  B+ grade and your marks is ",marks )
+}
+else if (marks>=70&&marks<75)
+{
+    document.write("you got the  B grade and your marks is ",marks )
+}
+else if (marks>=65&&marks<70)
+{
+    document.write("you got the  C+ grade and your marks is ",marks )
+}
+else if (marks>=60&&marks<65)
+{
+    document.write("you got the  C grade and your marks is ",marks )
+}
+else if (marks>=55&&marks<60)
+{
+    document.write("you got the  D+ grade and your marks is ",marks )
+}
+else if (marks>=50&&marks<55)
+{
+    document.write("you got the  D grade and your marks is ",marks )
+}
+else if (marks<50&&marks>=0)
+{
+    document.write("you got the  f grade try again ",marks )
+}
+else{
+    document.write(" you enter invalid data ")
+}
 
-if(hour>=5&&hour<=12)
-{
-    document.write("good morning");
-}
-else if(hour>=13&&hour<=17)
-{
-    document.write("good afternoon");
-}
- else if(hour>=18&&hour<=21)
-{
-    document.write("good evening");
-}
-else if(hour>=22&&hour<=24)
-{
-    document.write("this is night time");
-}
-else if(hour<=4&&hour>=1)
-{
-    document.write("this is night time");
-}
-else if (hour>24)
-{
-    document.write("not vaild time");
 }
